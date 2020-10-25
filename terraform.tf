20c11db7acd64df9cabdbcab7a2e725a0736bee7
KatsukiFujimoto <kf1996cnbl@gmail.com>
2020-10-25 23:47:43 +0900

    Remove: remove unenecessary line

--- terraform.tf
+++ terraform.tf
@@ -3,8 +3,6 @@ terraform {
     bucket     = "aws-ec2-terraform-tutorial" # should be created before
     key        = "dev/terraform.tfstate"
     region     = "ap-northeast-1"
-    access_key = var.access_key
-    secret_key = var.secret_key
   }
 }
 
